4af55caecf73f76d1a46c6c7dca0d42626876b68
ingvildfo <148064138+ingvildfo@users.noreply.github.com>
2023-11-20 14:04:27 +0100

    adding files

---
+++ B033/preprocessing.py
@@ -0,0 +1,68 @@
+"""Setup of preprocessing.
+
+This file is supposed to be a template for the BSc/DarSIA calibration project.
+
+Baseline image:
+Folder: \\klient.uib.no\FELLES\LAB-IT\IFT\resfys\FluidFlower\FF AB original data\Bilbo 030-on VTC chemistry series\B033 CO2 BTB 0.75 mM
+
+DSC46000.JPG
+
+"""
+
+# ! ---- IMPORTS ---- !
+
+import json
+from datetime import datetime
+from pathlib import Path
+
+import darsia
+
+# ! ---- DATA MANAGEMENT ---- !
+
+# Define single baseline image
+baseline_folder = "baseline_image"
+baseline_path = list(sorted(Path(baseline_folder).glob("*.JPG")))[0]
+
+# ! ---- UNMODIFIED BASELINE IMAGE ---- !
+original_baseline = darsia.imread(baseline_path)
+
+# ! ---- CORRECTION MANAGEMENT ---- !
+
+# Idea: Apply three corrections:
+# 1. Drift correction aligning images by simple translation with respect to teh color checker.
+# 2. Color correction applying uniform colors in the color checker.
+# 3. Curvature correction to crop images to the right rectangular format.
+# The order has to be applied in later scripts as well.
+
+# Start with empty config file.
+config = {}
+
+# 1. Drift correction: Define ROI corresponding to colorchecker
+point_selector = darsia.PointSelectionAssistant(original_baseline)
+config["drift"] = {}
+config["drift"]["roi"] = point_selector()
+# Later use: drift_correction = darsia.DriftCorrection(baseline_darsia_image, **config["drift"])
+
+# 2. Color correction: Mark the four corners of the color checker
+point_selector = darsia.PointSelectionAssistant(original_baseline)
+config["color"] = {}
+config["color"]["roi"] = point_selector()
+# Later use: color_correction = darsia.ColorCorrection(**config["drift"])
+
+# 3. Curvature correction: Crop image mainly. It is based on the unmodifed baseline image. All
+# later images are assumed to be aligned with that one. Bulge effects are neglected.
+crop_assistant = darsia.CropAssistant(original_baseline)
+config["curvature"] = crop_assistant()
+# Later use: curvature_correction = darsia.CurvatureCorrection(config=config["crop"])
+
+# ! ---- STORE CONFIG TO FILE ---- !
+
+# Before storing, need to make dict json serializable
+config["drift"]["roi"] = config["drift"]["roi"].tolist()
+config["color"]["roi"] = config["color"]["roi"].tolist()
+
+# Store config to file and use current datetime as ID
+date = datetime.now().strftime("%Y-%m-%d %H%M")
+Path("config").mkdir(exist_ok=True)
+with open(Path(f"config/preprocessing_{date}.json"), "w") as output:
+    json.dump(config, output)
